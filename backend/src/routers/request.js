a4ebde965f8e06e68f092a9671bffaafe432e10e
LeelaRohith <leelarohith10@gmail.com>
2025-01-24 15:03:22 +0530

    Started frontend React Application

--- backend/src/routers/request.js
+++ backend/src/routers/request.js
@@ -49,4 +49,34 @@ requestRouter.post(
     }
   }
 );
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed" });
+      }
+      const request = await connectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      if (!request) {
+        return res
+          .status(404)
+          .json({ message: "Connection Request not found" });
+      }
+      request.status = status;
+      const data = await request.save();
+      res.json({ message: "Connection request " + status, data });
+    } catch (err) {
+      res.status(400).send("ERROR " + err.message);
+    }
+  }
+);
 module.exports = requestRouter;
